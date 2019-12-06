e3bfa8e27e05960634d19962d129856fe5c689d4
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2019-12-06 17:17:31 +0000

    CRUD

---
+++ web/app/modules/admin/views/manufacturer/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Manufacturer */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="manufacturer-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'country')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
