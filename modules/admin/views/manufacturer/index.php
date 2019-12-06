772cfec54d8b5b8031fb68f6a51637c836e3631c
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2019-12-06 17:37:33 +0000

    CRUD

--- modules/admin/views/manufacturer/index.php
+++ modules/admin/views/manufacturer/index.php
@@ -1,9 +1,38 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>manufacturer/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ManufacturerSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Manufacturers';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="manufacturer-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Manufacturer', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'country',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
