255e07de30ea8b0871f0b7c4e7108de9c34b2312
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2019-12-06 16:12:57 +0000

    Module admin, CRUD

---
+++ web/app/modules/admin/views/Item/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Item */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="item-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'manufacturer_id')->textInput() ?>
+
+    <?= $form->field($model, 'pack_id')->textInput() ?>
+
+    <?= $form->field($model, 'sku')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'active')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
