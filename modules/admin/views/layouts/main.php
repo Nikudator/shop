9c9dd65d89c9971c6237f066b09ddfcb33fab7fb
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2019-12-09 11:21:19 +0700

    Admin Theme

--- modules/admin/views/layouts/main.php
+++ modules/admin/views/layouts/main.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use dmstr\web\AdminLteAsset;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -22,7 +23,7 @@ if (Yii::$app->controller->action->id === 'login') {
         app\assets\AppAsset::register($this);
     }
 
-    \dmstr\web\AdminLteAsset::register($this);
+    AdminLteAsset::register($this);
 
     $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
     ?>
